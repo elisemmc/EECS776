9dda17157667c81e0de8626b0549b537001a7bec
elistax9 <e908m429@ku.edu>
2016-10-18 16:59:41 -0500

    Guess a Number homework complete

---
+++ NumGuess.hs
@@ -0,0 +1,39 @@
+import Data.Char
+import System.Random
+
+generateNum :: IO Int
+generateNum = getStdRandom (randomR (1,10))
+
+playAgain :: IO ()
+playAgain = do
+    putStrLn "Play Again? [Y/N]"
+    answer <- getLine
+    if ( (answer == "Y") || (answer == "y") )
+        then do
+            putStrLn "Starting New Game"
+            main
+        else do
+            putStrLn " ... Exiting ... "
+
+guessNum :: (Ord a, Read a) => a -> IO ()
+guessNum num = do
+    putStrLn "Please guess a number"
+    guess <- getLine
+    if ( (read guess) == num )
+        then do
+            print "Correct"
+            playAgain
+        else
+            if ( (read guess) < num )
+                then do
+                    putStrLn "Higher"
+                    guessNum num
+                else do
+                    putStrLn "Lower"
+                    guessNum num
+
+main :: IO ()
+main = do
+    num <- generateNum
+    guessNum num
+    
